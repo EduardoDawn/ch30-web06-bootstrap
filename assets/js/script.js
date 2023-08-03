13eba197383d4f33d201931147168f280ff4a74a
EduardoPerez <eduperdawn@gmail.com>
2023-08-03 17:23:22 -0600

    feat: change color en h3

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,10 @@ console.log("Página en construcción con Patricio Estrella");
 function clickPatricio(){
     alert("Patricio lo esta intentando!!");
 }
+
+function changeColor(elementHtml, color){
+    //alert("Que emocion voy a cambiar mi color");
+   // console.log(color);
+
+    elementHtml.style.color = color;
+}
