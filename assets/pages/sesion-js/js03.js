a0ab09f815b2f66211cf7cdbaa79036c50abcd22
EduardoPerez <eduperdawn@gmail.com>
2023-08-09 11:06:09 -0600

    Declaración de bloque de código

--- assets/pages/sesion-js/js03.js
+++ assets/pages/sesion-js/js03.js
@@ -1,2 +1,54 @@
 console.log("Sesion JS03 Control de flujo");
 console.log("No olvidaremos el reto a Ed");
+
+/* 
+-------------------Declaración de bloque de código ---------------
+Sintaxis:
+    {
+
+    }
+Las variables declaradas con let y const solo tendrán alcance(scope)
+dentro del bloque o bloques anidados.
+
+Si la variable en el bloque, tiene el mismo nombre que una variable
+fuera del bloque, se le da prioridad a la variable dentro del 
+bloque
+
+*/
+
+let firstname = "Sergio";
+const lastname = "Torres";
+var age = "39";
+
+//let y const no se pueden redeclarar
+/* let firstname = "Sergio";
+const lastname = "Torres"; */
+//var age = "32";  //var se puede redeclarar
+{
+    let firstname = "Mau";
+    const lastname = "Peniche";
+    var age = "26";
+    const colorFavorito = "azul";
+    var comidaFavorita = "pozole";
+
+
+    console.log(`${firstname} ${lastname}`);
+    console.log(age);
+    console.log(comidaFavorita);
+    console.log(colorFavorito);
+    {
+        const colorFavorito = "morado";
+        console.log(colorFavorito);
+        console.log(firstname);
+        {
+            console.log(colorFavorito);
+            console.log(firstname);
+
+        }
+    }
+}
+
+console.log(`${firstname} ${lastname}`);
+console.log(age);
+console.log(comidaFavorita);
+// console.log(colorFavorito); //colorFavorito no está definido
